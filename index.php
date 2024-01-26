3e984aab2e9de39542008b313055a3a4e5461728
Gabriel Costa <gabrielsantoscosta005@gmail.com>
2024-01-25 23:53:53 -0300

    mudança de name categoria navbar

--- index.php
+++ index.php
@@ -39,7 +39,7 @@
      ----------------------------------------------------- -->      
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Modelos
+            Exercícios
           </a>
           <ul class="dropdown-menu">
             <li><a class="dropdown-item" href="?page=modelo-listar">Listar</a></li>
