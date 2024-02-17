a8cc52256a9fa9e33676adb2cd62fe0b81027cb2
Gabriel Costa <gabrielsantoscosta005@gmail.com>
2024-02-17 00:47:23 -0300

    alteração favicons

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>GoGym</title>
-    <link rel="shortcut icon" href="img/favicon-gogym.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="img/faviconGogym.png" type="image/x-icon">
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 </head>
 <body>
